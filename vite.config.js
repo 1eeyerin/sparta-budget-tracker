d4bfee47329b5695b6e890628fee5e7882a79cd8
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-05-24 22:17:44 +0900

    refactor: shadcn-ui & react-hook-form 제거

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/sparta-budget-tracker/",
   plugins: [react()],
